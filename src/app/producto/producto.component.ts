47f259d3a19abacd19f43e0ff025ad724078134b
Estrella Iveth Flores Lucas <estrellaflores550@gmail.com>
2025-02-25 18:04:48 -0600

    Creacion de componentes dentro de un componente

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -10,9 +10,20 @@ import { Component } from '@angular/core';
 
 export class ProductoComponent {
 
+    public nombre: string;
+    public marca: string;
+    public precio: number;
+    public descripcion: string;
+
     constructor(){
+
+        this.nombre = "PC PORTATIL";
+        this.marca = "Lenovo";
+        this.precio = 1500.00;
+        this.descripcion = "Una PC portatil de 15.6 pulgadas con procesador Intel Core i7 y RAM de 16GB";
+
         console.log("Se ha cargado el componente de mi de productos")
-        alert("HOLAAA");
+        
     }
 
 }
